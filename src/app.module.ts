6d33d314a8809e26d424ffc19533ff22ab388403
dipeshpawar118 <dipeshpawar118@gmail.com>
2022-11-24 14:49:56 +0530

    Added user Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { DogModule } from './dog/dog.module';
 import { LoggerMiddleware } from './middleware/log.middleware';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [CatsModule, DogModule],
+  imports: [CatsModule, DogModule , MongooseModule.forRoot('mongodb://localhost:27017/nest'), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
